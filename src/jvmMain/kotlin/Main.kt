b22d025bb34c08b9160b92cb062929e291337d32
Cosmin <tudor_cosmin98@yahoo.com>
2024-04-06 23:04:03 +0300

    transition between screens

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -19,77 +19,74 @@ import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 
 
-class BaseScreen : Screen {
+@Composable
+fun SharedImage(imageUrl: String, orbitalScope: OrbitalScope) {
+    KamelImage(
+        resource = asyncPainterResource(data = imageUrl),
+        contentDescription = "",
+        modifier = Modifier
+            .animateSharedElementTransition(
+                orbitalScope,
+                SpringSpec(stiffness = 200f, dampingRatio = Spring.DampingRatioLowBouncy),
+                SpringSpec(stiffness = 200f, dampingRatio = Spring.DampingRatioLowBouncy)
+            )
+    )
+}
+
+class ScreenA : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
 
+        val sharedImages = listOf(
+            "https://upload.wikimedia.org/wikipedia/commons/2/28/030106-IMG_0819-HozMarreh-2.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/7/72/Beautiful_scenery_in_Attappadi.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/e/ea/030106-IMG_0700-Qom-Garmsar-2.jpg"
+        ).map { image ->
+            rememberContentWithOrbitalScope {
+                SharedImage(image, this)
+            }
+        }
+
         Button(onClick = {
             navigator.push(
-                ScreenA(
-                    listOf(
-                        "https://miro.medium.com/v2/resize:fit:1400/1*QjQTzohDz9gznswKpuqUXQ.jpeg",
-                        "https://miro.medium.com/v2/resize:fit:1400/1*QjQTzohDz9gznswKpuqUXQ.jpeg",
-                        "https://miro.medium.com/v2/resize:fit:1400/1*QjQTzohDz9gznswKpuqUXQ.jpeg"
-                    )
-                )
+                ScreenB(sharedImages)
             )
         }) {
-            Text("Go To Screen A")
+            Text("Go To Screen B")
         }
     }
 }
 
-
-@Composable
-fun SharedImage(imageUrl: String, modifier: Modifier, orbitalScope: OrbitalScope) {
-    Box(modifier = modifier.aspectRatio(1.77f)) {
-            KamelImage(
-                resource = asyncPainterResource(data = imageUrl),
-                contentDescription = "",
-                modifier = Modifier
-                    .animateSharedElementTransition(
-                        orbitalScope,
-                        SpringSpec(stiffness = 500f, dampingRatio = Spring.DampingRatioMediumBouncy),
-                        SpringSpec(stiffness = 500f, dampingRatio = Spring.DampingRatioMediumBouncy)
-                    )
-            )
-    }
-}
-
-data class ScreenA(val images: List<String>) : Screen {
+data class ScreenB(val images: List<@Composable() (OrbitalScope.() -> Unit)>) : Screen {
 
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-
-        Orbital {
-            Column {
-                for (image in images) {
-                    val shareImage = rememberContentWithOrbitalScope {
-                        SharedImage(image, Modifier.width(200.dp), this)
-                    }
-                    Box(Modifier.onClick { navigator.push(ScreenB(shareImage)) }) {
-                        shareImage()
-                    }
-
+        Column {
+            Button(onClick = { navigator.pop() }) {
+                Text("Go to Screen A")
+            }
+            for (image in images) {
+                Orbital(Modifier.width(200.dp).aspectRatio(1.77f).onClick { navigator.push(ScreenC(image)) }) {
+                    image()
                 }
             }
         }
     }
 }
 
-data class ScreenB(val sharedContent: @Composable() (OrbitalScope.() -> Unit)) : Screen {
+data class ScreenC(val sharedContent: @Composable() (OrbitalScope.() -> Unit)) : Screen {
 
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         Column(modifier = Modifier.width(300.dp)) {
             Button(onClick = { navigator.pop() }) {
-                Text("Go to Screen A")
+                Text("Go to Screen B")
             }
-            Orbital {
+            Orbital(modifier = Modifier.width(500.dp)) {
                 sharedContent()
             }
         }
@@ -98,7 +95,6 @@ data class ScreenB(val sharedContent: @Composable() (OrbitalScope.() -> Unit)) :
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-
-        Navigator(BaseScreen())
+        Navigator(ScreenA())
     }
 }
